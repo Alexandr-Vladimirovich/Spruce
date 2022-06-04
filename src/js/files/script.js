7a182ea5ae69ebe5ffcee60476689695e2bfc3a9
Denis Sinyukov <dnsinyukov@gmail.com>
2022-06-04 22:51:22 +0300

    set content styles - Article format

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,23 +1,23 @@
-// Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
-// Подключение списка активных модулей
-import { flsModules } from "./modules.js";
-
-
-const link=document.querySelectorAll('p> a');
-link.forEach(element => {
-	element.style.color='#008fb9';
-});
-const titleTwo=document.querySelectorAll('h2');
-titleTwo.forEach(element => {
-	element.style.fontSize='1.5rem';
-});
-const parText=document.querySelectorAll('p');
-parText.forEach(element => {
-	element.style.fontSize='20px';
-});
-const titleVan=document.querySelectorAll('h1');
-titleVan.forEach(element => {
-	element.style.fontSize='50px';
-});
-
+// // Подключение функционала "Чертогов Фрилансера"
+// import { isMobile } from "./functions.js";
+// // Подключение списка активных модулей
+// import { flsModules } from "./modules.js";
+//
+//
+// const link=document.querySelectorAll('p> a');
+// link.forEach(element => {
+// 	element.style.color='#008fb9';
+// });
+// const titleTwo=document.querySelectorAll('h2');
+// titleTwo.forEach(element => {
+// 	element.style.fontSize='1.5rem';
+// });
+// const parText=document.querySelectorAll('p');
+// parText.forEach(element => {
+// 	element.style.fontSize='20px';
+// });
+// const titleVan=document.querySelectorAll('h1');
+// titleVan.forEach(element => {
+// 	element.style.fontSize='50px';
+// });
+//
